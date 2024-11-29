d747eb1c9fc4f03a26b401c7eaf4c62c8febf292
Cristian Bucheli <119817349+AIDAN535@users.noreply.github.com>
2024-11-29 18:53:28 -0500

    Creación archivo main

---
+++ main.py
@@ -0,0 +1,12 @@
+# Programa básico en Python para multiplicar dos números
+print("Bienvenido al programa de multiplicación")
+
+# Números quemados en el código
+numero1 = 7
+numero2 = 3
+
+# Calcular la multiplicación
+resultado = numero1 * numero2
+
+# Mostrar el resultado
+print(f"La multiplicación de {numero1} por {numero2} es: {resultado}")
