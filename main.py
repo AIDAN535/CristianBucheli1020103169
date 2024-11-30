68f7d91f37980bb4017b4fa685ac6a41411a4da0
Cristian Bucheli <119817349+AIDAN535@users.noreply.github.com>
2024-11-29 19:56:02 -0500

    Commit 2

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ resultado = numero1 * numero2
 # Mostrar el resultado
 print(f"La multiplicación de {numero1} por {numero2} es: {resultado}")
 print("Buenas noches profesor")
+print("Espero que se encuentre muy bien")
